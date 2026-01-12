1fbcf59c935f6892e8a86aea3973818abd3e1538
Jorge Suárez <jorgealejandrosuarez0807@gmail.com>
2026-01-12 16:58:38 -0400

    Arreglar el ghpages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://jasr2003.github.io/SitioWebPersonal-V2/'
+  base: '/SitioWebPersonal-V2/'
 })
